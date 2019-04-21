2fa00c41b6c4a1c234a14406a5d7cf0faff716e9
Jmackas <jamesrossmackay@gmail.com>
2019-04-21 11:39:55 +0800

    Second for loop added

--- moodle_time/script.js
+++ moodle_time/script.js
@@ -3,36 +3,42 @@
 timezoneCalc();
 
 function timezoneCalc() {
-  let webinarTable = document.querySelectorAll("#webinartable td");
+    let webinarTable = document.querySelectorAll("#webinartable td");
 
-  // Detect all instances of the time zone in the first column
-  let webinarFirstCol = document.querySelectorAll("#webinartable td:nth-of-type(1)");
+    // Detect all instances of the time zone in the first column
+    let webinarFirstCol = document.querySelectorAll("#webinartable td:nth-of-type(1)");
 
-  // Identify all instances of the time zone to be updated in the second column
-  let webinarSecondCol = document.querySelectorAll("#webinartable td:nth-of-type(2)");
+    // Identify all instances of the time zone to be updated in the second column
+    let webinarSecondCol = document.querySelectorAll("#webinartable td:nth-of-type(2)");
 
-  // Find user timezone
-  let timezoneDiff = new Date().getTimezoneOffset();
+    // Find user timezone
+    let timezoneDiff = new Date().getTimezoneOffset();
 
-  // Convert to hours
-  timezoneDiff = Math.abs(timezoneDiff / 60);
+    // Convert to hours
+    timezoneDiff = Math.abs(timezoneDiff / 60);
 
-  // Convert to epoch
-  timezoneDiff = timezoneDiff * 3600000;
+    // Convert to epoch
+    timezoneDiff = timezoneDiff * 3600000;
 
-  // Loop through all values in the first column of the table and convert times
-  for (let i = 1; i < webinarFirstCol.length; i++) {
-    let finalDiff;
+    // Loop through all values in the first column of the table and convert times
+    for (let i = 1; i < webinarFirstCol.length; i++) {
+        let finalDiff;
 
-    // Convert table cell from text to int (hacky method - .value didn't work). Also convert date to Epoch time.
-    finalDiff = parseInt(new Date(webinarFirstCol[i].innerHTML).getTime());
+        // Convert table cell from text to int (hacky method - .value didn't work). Also convert date to Epoch time.
+        finalDiff = parseInt(new Date(webinarFirstCol[i].innerHTML).getTime());
 
-    // Add the timezone difference
-    finalDiff = finalDiff + timezoneDiff;
+        // Add the timezone difference
+        finalDiff = finalDiff + timezoneDiff;
 
-    //   webinarFirstCol[i].innerHTML + new Date(finalDiff).innerHTML;
-    console.log(finalDiff);
+        //   webinarFirstCol[i].innerHTML + new Date(finalDiff).innerHTML;
+        console.log(finalDiff);
 
+        for (let i = 1; i < webinarSecondCol.length; i++) {
+            let inputDiff;
 
-  }
+            webinarSecondCol[i].innerHTML = new Date(finalDiff);
+        }
+
+    }
+
 }
