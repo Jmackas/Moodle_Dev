1bb9bf5ac535492e4256e98021ecdad81ed0a571
Jmackas <jamesrossmackay@gmail.com>
2019-05-14 17:42:22 +0800

    User detected script functioning

--- admin_interface/scripts.js
+++ admin_interface/scripts.js
@@ -1,10 +1,17 @@
 
+detectUser();
+
 // Detect the 
 function detectUser () {
-    if () {
+    // Detect user's name
+    let userName = document.querySelector(".usermendrop").innerText;
+
+    // If the user is the following name
+     if (userName = "James Mackay") {
+        // Insert button for user if manager
         let targettedNode = document.querySelector("ul.dropdown-menu.usermen");
         targettedNode.insertAdjacentHTML('beforeend', '<li><button onclick="activateAdmin()">Activate Admin Mode</button></li>');
-    }
+    } 
     
 }
 
