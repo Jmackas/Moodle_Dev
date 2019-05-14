81f3faf6fa03fdd9d0cb22c05b98268afec23f7f
Jmackas <jamesrossmackay@gmail.com>
2019-05-14 17:52:01 +0800

    Array of users developed

--- admin_interface/scripts.js
+++ admin_interface/scripts.js
@@ -6,8 +6,11 @@ function detectUser () {
     // Detect user's name
     let userName = document.querySelector(".usermendrop").innerText;
 
-    // If the user is the following name
-     if (userName = "James Mackay") {
+    // Array of managers to confirm. Note - add a space at the end of each name
+    var managers = ["James Mackay ", "Natalie Deng"];
+
+    // If the user is in the following array
+     if (managers.includes(userName) == true) {
         // Insert button for user if manager
         let targettedNode = document.querySelector("ul.dropdown-menu.usermen");
         targettedNode.insertAdjacentHTML('beforeend', '<li><button onclick="activateAdmin()">Activate Admin Mode</button></li>');
