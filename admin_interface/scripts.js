05d083aed8ab33555d5cad14c9e8ae433c549dde
Jmackas <jamesrossmackay@gmail.com>
2019-05-14 17:33:22 +0800

    Add button to user interface

---
+++ admin_interface/scripts.js
@@ -0,0 +1,14 @@
+
+// Detect the 
+function detectUser () {
+    if () {
+        let targettedNode = document.querySelector("ul.dropdown-menu.usermen");
+        targettedNode.insertAdjacentHTML('beforeend', '<li><button onclick="activateAdmin()">Activate Admin Mode</button></li>');
+    }
+    
+}
+
+// Activate admin mode
+function activateAdmin() {
+    console.log("worked!");
+}
