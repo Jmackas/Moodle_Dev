48446d2f546eb2a616c39c4079424f142516476f
Jmackas <jamesrossmackay@gmail.com>
2019-06-12 20:25:08 +0800

    Sort function update

--- moodle_dropdown_sort/scripts.js
+++ moodle_dropdown_sort/scripts.js
@@ -5,9 +5,9 @@ invokeSort();
 // Check if the grades module is active
 function invokeSort() {
 
-        let entireDoc = document.getElementById('graded_users_selector');
-        entireDoc.insertAdjacentHTML('afterend', '<button onclick="sortListAlph()" class="bttn">Sort First Name</button>');
-        entireDoc.insertAdjacentHTML('afterend', '<br>');
+    let entireDoc = document.getElementById('graded_users_selector');
+    entireDoc.insertAdjacentHTML('afterend', '<button onclick="sortListAlph()" class="bttn">Sort First Name</button>');
+    entireDoc.insertAdjacentHTML('afterend', '<br>');
 
 }
 
@@ -17,14 +17,14 @@ function sortListAlph() {
     var cl = document.querySelector('[name="userid"]');
     var clTexts = new Array();
 
-    for (i = 2; i < cl.length; i++) {
-        clTexts[i - 2] = cl.options[i].text.toUpperCase() + "," + cl.options[i].text + "," + cl.options[i].value + "," + cl.options[i].selected;
+    for (i = 1; i < cl.length; i++) {
+        clTexts[i - 1] = cl.options[i].text.toUpperCase() + "," + cl.options[i].text + "," + cl.options[i].value + "," + cl.options[i].selected;
     }
 
     clTexts.sort();
 
-    for (i = 2; i < cl.length; i++) {
-        var parts = clTexts[i - 2].split(',');
+    for (i = 1; i < cl.length; i++) {
+        var parts = clTexts[i - 1].split(',');
 
         cl.options[i].text = parts[1];
         cl.options[i].value = parts[2];
